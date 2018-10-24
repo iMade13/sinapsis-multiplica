7202b0598866e3c67befe5b1f44d3261516fa8fc
Sofía Maturana <sofimatsu@gmail.com>
2018-10-24 11:05:46 -0300

    gulp- sass  config

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify');
+
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function() {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
