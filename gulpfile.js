05683e2448dec77f36525afccbfb522579f9a237
Sofía Maturana <sofimatsu@gmail.com>
2018-10-24 11:11:19 -0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,11 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function() {
-    return gulp.src('./sass/**/*.scss')
+    return gulp.src('./assets/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./assets/css'));
 });
 
 gulp.task('sass:watch', function() {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./assets/sass/**/*.scss', ['sass']);
 });
