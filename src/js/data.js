40c64533afd41b2b8ba4825afe4d76d3baddb2a3
Camila <camipereira2111@gmail.com>
2018-10-25 15:12:36 -0300

    cambios cami

--- src/js/data.js
+++ src/js/data.js
@@ -46,12 +46,6 @@ function dropdown(cities) {
     });
 };
 
-// var database = firebase.database();
-// var userId = firebase.auth().currentUser.uid;
-// return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-//   var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-//   // ...
-// });
 
 
 
@@ -65,62 +59,68 @@ function dropdown(cities) {
 //     }
 // }
 
+function showCourses() {
+    firebase.database().getElementById('dscrp-cursos').orderByChild("titulo").on('child_added', (post) => {
+                let userKeyPost = post.val().userUid
+                firebase.database().ref(`users/${userKeyPost}`)
+                    .once('value')
+
+            }
+
+            function filterTopics() {
 
 
 
+            }
+            const topics = 'api/topic.json';
+            const city = "api/city.json";
 
-function filterTopics() {
+            // function getData(url) {
+            //     return fetch(url, {
+            //             method: "GET",
+            //             headers: {
+            //                 'Accept': 'application/json',
+            //                 'Content-Type': 'application/json',
+            //             },
+            //         })
+            //         .then((response) => response.json())
+            //         .then((responseData) => {
+            //             // console.warn(responseData);
+            //             console.log(responseData);
+            //             return responseData;
 
+            //         })
+            //         .catch(error => console.warn(error));
+            // }
 
-}
-const topics = 'api/topic.json';
-const city = "api/city.json";
+            // getData(topics).then(response => {
+            //     autocomplete(document.getElementById("inputTemas"), response.topics)
+            // });
+            // getData(city).then(response => {
+            //     autocomplete(document.getElementById("dropdownSede"), response.city.name)
+            // });
 
-// function getData(url) {
-//     return fetch(url, {
-//             method: "GET",
-//             headers: {
-//                 'Accept': 'application/json',
-//                 'Content-Type': 'application/json',
-//             },
-//         })
-//         .then((response) => response.json())
-//         .then((responseData) => {
-//             // console.warn(responseData);
-//             console.log(responseData);
-//             return responseData;
+            function dropdown(city) {
+                city.forEach(element => {
+                    console.log();
+                    let contenedor = document.getElementById('dropdownSede');
+                    let h1 = document.getElementById('enabled');
+                    let id = element.id;
+                    console.log(city.id);
+                    // let template = ` < button class = "dropdown-item"
+                    type = "button" > $ { id } < /button>`;
+                    let btn = document.createElement('button');
+                    if (name === 'Santiago de Chile') {
+                        btn.className = 'dropdown-item';
+                        btn.setAttribute('onclick');
+                    } else {
+                        btn.className = 'dropdown-item disabled';
+                    };
+                    btn.type = 'button';
+                    let text = document.createTextNode(id);
+                    // console.log(text)
+                    btn.appendChild(text);
+                    contenedor.appendChild(btn);
 
-//         })
-//         .catch(error => console.warn(error));
-// }
-
-// getData(topics).then(response => {
-//     autocomplete(document.getElementById("inputTemas"), response.topics)
-// });
-// getData(city).then(response => {
-//     autocomplete(document.getElementById("dropdownSede"), response.city.name)
-// });
-
-function dropdown(city) {
-    city.forEach(element => {
-        console.log();
-        let contenedor = document.getElementById('dropdownSede');
-        let h1 = document.getElementById('enabled');
-        let id = element.id;
-        console.log(city.id);
-        // let template = `<button class="dropdown-item" type="button">${id}</button>`;
-        let btn = document.createElement('button');
-        if (name === 'Santiago de Chile') {
-            btn.className = 'dropdown-item';
-            btn.setAttribute('onclick');
-        } else {
-            btn.className = 'dropdown-item disabled';
-        };
-        btn.type = 'button';
-        let text = document.createTextNode(id);
-        // console.log(text)
-        btn.appendChild(text);
-        contenedor.appendChild(btn);
-
-    })
-};
+                })
+            };
