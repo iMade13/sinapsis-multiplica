84b51d4b1475c48e9db3d57b001f0afa4a5b589f
Kaybel <kblmrtnz@gmail.com>
2018-10-25 12:57:44 -0300

    estilos

--- src/js/toggle.js
+++
@@ -1,7 +0,0 @@
-$( "card-header" ).toggle(
-  function() {
-    $( this ).addClass( "background-color: #92a8d1;" );
-  }, function() {
-    $( this ).removeClass( "background-color: #92a8d1;" );
-  }
-);
